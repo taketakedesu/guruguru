f1653b0460e62ee9ce8da9e594103f5582c4a130
taketakedesu <ketaketadesu@gmail.com>
2020-10-08 23:04:43 +0900

    bootstrap

--- guruguru/views.py
+++ guruguru/views.py
@@ -6,6 +6,7 @@ from .forms import PostForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'guruguru/post_list.html', {'posts': posts})
--- gurutake/settings.py
+++ gurutake/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'gurutake',
-        'USER': 'name',
+        'USER': 'guruguru',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
